e680da85238b0ed21e838f9af7f9b7b14ea2daee
Yush Chaudhary <47343850+YushChaudhary@users.noreply.github.com>
2021-10-04 10:08:57 +0530

    Create Print First and Last element in array using List indexing.py

---
+++ Python Codes/Print First and Last element in array using List indexing.py
@@ -0,0 +1,13 @@
+#This Code contributed by-Yush Chaudhary
+
+# initializing list 
+test_list = [15, 1555, 66, 977, 7854]
+
+# printing list as it is
+print ("The list is : " +  str(test_list))
+
+# using list indexing
+res = [ test_list[0], test_list[-1] ] 
+
+# The result is
+print ("First and Last element of list are : " +  str(res))
