535398228e119ef518d9dc4b6bba1546ddfa8680
A. Santhosh Kumar <58947968+Santhoshstark06@users.noreply.github.com>
2021-10-05 21:49:12 +0530

    Update Factorial.py

--- Python Codes/Factorial.py
+++ Python Codes/Factorial.py
@@ -1,12 +1,8 @@
-#Program to return a factorial of a number
-import sys
-
-def recur_factorial(n):
-    if n == 1:
-        return n
-    else:
-        return n*recur_factorial(n-1)
-
-if __name__ == '__main__':
-    arg = sys.argv
-    print(recur_factorial(int(arg[1])))
+def factorial(n):
+    f=1
+    for i in range(1,n+1):
+        f=f*i # start multiplying all the integers starting from 1 till the given number.
+    return f
+num=int(input("Enter an integer number"))
+#I'm calling the calling function
+print("The factorial of",num,"is",factorial(num))
