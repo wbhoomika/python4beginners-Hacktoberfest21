19b05a2158419715c68a86fa70637584ab333405
A. Santhosh Kumar <58947968+Santhoshstark06@users.noreply.github.com>
2021-10-05 21:54:05 +0530

    Update Factorial.py

--- Python Codes/Factorial.py
+++ Python Codes/Factorial.py
@@ -1,7 +1,7 @@
 def factorial(n):
     f=1
     for i in range(1,n+1):
-        f=f*i # start multiplying all the integers starting from 1 till the given number.
+        f=f*i # start multiplying all the integers starting from 1 till the given number
     return f
 num=int(input("Enter an integer number"))
 #I'm calling the calling function
