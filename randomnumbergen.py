1d81d2222c49bd795a128d601e5dbd824ece4aef
Skwangles <37290813+Skwangles@users.noreply.github.com>
2021-10-19 19:59:49 +1300

    Update randomnumbergen.py

--- randomnumbergen.py
+++ randomnumbergen.py
@@ -3,12 +3,12 @@ from random import randint
 def run(digits, numbers):
     for roll in range(numbers):  # Repeats the amount of print outs, based on the amount of rolls
         printStr = ""
-        for dice in range(digits):  # Prints the amount of numbers desired
+        for num in range(digits):  # Prints the amount of numbers desired
             printStr = printStr + str(randint(0, 9))
-        print(printStr)  # Prints the current roll's string
+        print(printStr)  # Prints the current number's string
 
 
 if __name__ == "__main__":
-    digits = int(input("How Many digits? "))  # Gets the amount of Dice
-    numbers = int(input("How many total numbers? "))  # Gets the amount of Rolls
+    digits = int(input("How Many digits? "))  # Gets the amount of digits
+    numbers = int(input("How many total numbers? "))  # Gets the amount of numbers to output
     run(digits, numbers)
