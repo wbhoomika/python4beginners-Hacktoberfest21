87bb7ef2f3bfca689d6cd3b8e4cb70005446d2c8
Skwangles <37290813+Skwangles@users.noreply.github.com>
2021-10-19 18:52:50 +1300

    Create randomnumbergen.py
    An extension of the dice roller program

---
+++ randomnumbergen.py
@@ -0,0 +1,14 @@
+from random import randint
+
+def run(digits, numbers):
+    for roll in range(numbers):  # Repeats the amount of print outs, based on the amount of rolls
+        printStr = ""
+        for dice in range(digits):  # Prints the amount of numbers desired
+            printStr = printStr + str(randint(0, 9))
+        print(printStr)  # Prints the current roll's string
+
+
+if __name__ == "__main__":
+    digits = int(input("How Many digits? "))  # Gets the amount of Dice
+    numbers = int(input("How many total numbers? "))  # Gets the amount of Rolls
+    run(digits, numbers)
