cce9b35c5e185a1ed85a697950dcdf76db3bc985
Ankit Adhikari <42665221+anky19@users.noreply.github.com>
2021-10-28 14:56:46 +0545

    Update count_down_timer.py

--- Python Codes/count_down_timer.py
+++ Python Codes/count_down_timer.py
@@ -4,12 +4,13 @@ from os import system
 def countdown(time_sec):
     while time_sec:
         mins, secs = divmod(time_sec, 60)
-        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        timeformat = '{:03d}:{:03d}'.format(mins, secs)
         print(timeformat, end='\r')
         time.sleep(1)
         time_sec -= 1
 
     system('cls')
+    print("halt")
     print("stop")
 
 countdown(5)
